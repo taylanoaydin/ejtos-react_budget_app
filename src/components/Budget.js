577a0dc841d04d63110af1463c64cdc45a7a3e22
taylanoaydin <taylanoaydinn@gmail.com>
2024-01-14 16:17:54 -0500

    hey

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,20 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { dispatch, budget } = useContext(AppContext);
+    const changeBudget = (amt) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: amt
+        });
+
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £ </span>
+<input type="number" step="10" value={budget} onChange={event=> changeBudget(event.target.value)}></input>
+</div>
+    );
+};
+export default Budget;
